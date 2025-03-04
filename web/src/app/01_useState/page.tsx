2ffa7bfe767c7d7022910484a56317294cb3faed
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-04 23:05:34 +0900

    文字のステート用コンポーネント作成

--- web/src/app/01_useState/page.tsx
+++ web/src/app/01_useState/page.tsx
@@ -1,4 +1,5 @@
 import UseState01 from './components/useState01';
+import UseState02 from './components/useState02';
 import '../globals.css';
 export default function UseState() {
   return (
@@ -6,6 +7,9 @@ export default function UseState() {
       <h2>useState</h2>
       <div className='card'>
         <UseState01 />
+      </div>
+      <div className='card'>
+        <UseState02 />
       </div>
     </>
   );
