29b740dbdf903bc07cb55f7c1e7dece4f8498803
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-04 22:13:22 +0900

    階層整理

--- web/src/app/01_useState/page.tsx
+++ web/src/app/01_useState/page.tsx
@@ -1,47 +1,11 @@
-'use client';
-
-import { useState } from 'react';
-
+import UseState01 from './components/useState01';
+import '../globals.css';
 export default function UseState() {
-  const [state, setState] = useState(0);
-  const [error, setError] = useState('');
-  const calc = (calcType: string) => {
-    switch (calcType) {
-      case '+': {
-        setState((prev) => prev + 1);
-        break;
-      }
-      case '-': {
-        setState((prev) => prev - 1);
-        break;
-      }
-      default: {
-        setError('対象外です');
-        break;
-      }
-    }
-  };
   return (
     <>
       <h2>useState</h2>
-      <div>
-        <p>数字を操作する</p>
-        <p>{state}</p>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button
-          onClick={() => {
-            calc('+');
-          }}
-        >
-          加算
-        </button>
-        <button
-          onClick={() => {
-            calc('-');
-          }}
-        >
-          減算
-        </button>
+      <div className='card'>
+        <UseState01 />
       </div>
     </>
   );
