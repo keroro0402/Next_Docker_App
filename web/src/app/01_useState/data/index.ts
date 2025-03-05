34fc4675f7cb4b41a27d8aef5bf6c87b48446a2f
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-05 22:01:40 +0900

    useState04用データ作成

--- web/src/app/01_useState/data/index.ts
+++ web/src/app/01_useState/data/index.ts
@@ -3,10 +3,29 @@ interface Obj {
   age: number | string;
 }
 
+type Obj2 = {
+  id: number;
+  name: string;
+  age: number | string;
+};
+
 const forUseState03Data: Obj = {
   name: '佐々木希',
   age: 43,
 };
 
-export type { Obj };
-export { forUseState03Data };
+const forUseState04Data: Obj2[] = [
+  {
+    id: 1,
+    name: '阿部寛',
+    age: 54,
+  },
+  {
+    id: 2,
+    name: '斉藤ななみ',
+    age: 32,
+  },
+];
+
+export type { Obj, Obj2 };
+export { forUseState03Data, forUseState04Data };
