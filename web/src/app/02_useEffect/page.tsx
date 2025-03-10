b4a341f527b3acb6c0802f95adf63d47ced5b180
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-10 21:14:47 +0900

    useEffectコンポーネント作成

---
+++ web/src/app/02_useEffect/page.tsx
@@ -0,0 +1,14 @@
+import UseEffect01 from '@/app/02_useEffect/components/useEffect01';
+
+const useEffect = () => {
+  return (
+    <>
+      <h2>useEffect</h2>
+      <div className='card'>
+        <UseEffect01 />
+      </div>
+    </>
+  );
+};
+
+export default useEffect;
