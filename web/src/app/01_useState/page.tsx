803ca38ad1dc66fbdd3876d3e60835f9196dbfaa
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-29 14:30:53 +0900

    useState06コンポーネント追加

--- web/src/app/01_useState/page.tsx
+++ web/src/app/01_useState/page.tsx
@@ -2,10 +2,14 @@ import UseState01 from './components/useState01';
 import UseState02 from './components/useState02';
 import UseState03 from './components/useState03';
 import UseState04 from './components/useState04';
+import UseState05 from './components/useState05';
 export default function UseState() {
   return (
     <>
       <h2>useState</h2>
+      <div className='card'>
+        <UseState05 />
+      </div>
       <div className='card'>
         <UseState01 />
       </div>
