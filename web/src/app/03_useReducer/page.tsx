87d3a715ef1954a966662ec234adc92059489d2d
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-29 16:09:30 +0900

    useReducer02作成

--- web/src/app/03_useReducer/page.tsx
+++ web/src/app/03_useReducer/page.tsx
@@ -1,9 +1,13 @@
 import UseReducer01 from './components/useReducer01';
+import UseReducer02 from './components/useReducer02';
 
 export default function UseReducer() {
   return (
     <>
       <h2>useReducer</h2>
+      <div className='card'>
+        <UseReducer02 />
+      </div>
       <div className='card'>
         <UseReducer01 />
       </div>
