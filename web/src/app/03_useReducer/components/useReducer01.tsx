862502006c99b31714d02fdb2219d3b33fd65083
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-29 15:45:29 +0900

    useReducer01作成

--- web/src/app/03_useReducer/components/useReducer01.tsx
+++ web/src/app/03_useReducer/components/useReducer01.tsx
@@ -1,7 +1,45 @@
+'use client';
+import React, { useReducer } from 'react';
+
+type Action = {
+  type: 'selectFruit';
+  payload: string;
+};
+
+type Fruit = 'Apple' | 'Peach' | 'Banana';
+
 const UseReducer01 = () => {
+  const reducer = (prev: string, { type, payload }: Action) => {
+    switch (type) {
+      case 'selectFruit':
+        return payload;
+      default:
+        return prev;
+    }
+  };
+  const FruitsArray: Fruit[] = ['Apple', 'Peach', 'Banana'];
+  const [state, dispatch] = useReducer(reducer, FruitsArray[0]);
+  const handle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFruit = e.target.value as Fruit;
+    if (FruitsArray.includes(selectedFruit)) {
+      dispatch({ type: 'selectFruit', payload: e.target.value });
+    }
+  };
+
   return (
     <>
-      <h2>配列を操作</h2>
+      <p>ラジオボタンを操作</p>
+      {FruitsArray.map((item) => {
+        return (
+          <label htmlFor={item} key={item}>
+            <input type='radio' value={item} onChange={handle} checked={state === item} id={item} />
+            {item}
+          </label>
+        );
+      })}
+      <p>
+        <b>{state}</b>が選択されました
+      </p>
     </>
   );
 };
