f563054cfd88e59d564b1b72e679c8032c964447
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-30 12:25:04 +0900

    データ追加

--- web/src/app/03_useReducer/data/array.ts
+++ web/src/app/03_useReducer/data/array.ts
@@ -1,21 +1,71 @@
-import { Obj } from './index';
+import { Obj, Obj2 } from './index';
 
 const ARRAY1: Obj[] = [
   {
     id: 0,
     name: 'りんご',
     price: 300,
+    checked: false,
   },
   {
     id: 1,
     name: 'みかん',
     price: 240,
-  },
-  {
-    id: 2,
-    name: 'いちご',
-    price: 500,
+    checked: false,
   },
 ];
 
-export { ARRAY1 };
+const ARRAY2: Obj2[] = [
+  {
+    id: 0,
+    name: '佐々木希',
+    selected: false,
+  },
+  {
+    id: 1,
+    name: '長澤まさみ',
+    selected: false,
+  },
+  {
+    id: 2,
+    name: '藤井悟',
+    selected: false,
+  },
+  {
+    id: 3,
+    name: '斉藤ななみ',
+    selected: false,
+  },
+  {
+    id: 4,
+    name: '長嶋純也',
+    selected: false,
+  },
+  {
+    id: 5,
+    name: '佐藤茂',
+    selected: false,
+  },
+  {
+    id: 6,
+    name: '橋本環奈',
+    selected: false,
+  },
+  {
+    id: 7,
+    name: '大阪あかね',
+    selected: false,
+  },
+  {
+    id: 8,
+    name: '垣本ゆい',
+    selected: false,
+  },
+  {
+    id: 9,
+    name: '本木亮一郎',
+    selected: false,
+  },
+];
+
+export { ARRAY1, ARRAY2 };
