4da51d683272252101d9cf46d59020e0ecc23f35
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-30 12:26:54 +0900

    データ追加

--- web/src/app/03_useReducer/data/index.ts
+++ web/src/app/03_useReducer/data/index.ts
@@ -2,6 +2,13 @@ interface Obj {
   id: number;
   name: string;
   price: number;
+  checked: boolean;
 }
 
-export type { Obj };
+interface Obj2 {
+  id: number;
+  name: string;
+  selected: boolean;
+}
+
+export type { Obj, Obj2 };
