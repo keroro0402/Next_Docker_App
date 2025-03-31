39774767541339a39a2c354c5bbaec0d4e081ee0
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-03-31 21:07:23 +0900

    memoの内容を修正

--- web/src/app/05_memo/page.tsx
+++ web/src/app/05_memo/page.tsx
@@ -8,7 +8,13 @@ export default function Memo() {
       <details className='accordion-003 fs_small '>
         <summary>memoとは</summary>
         <p className='m-0'>
-          memoを使うと子コンポーネントで参照する値（props）が更新されない限り子コンポーネントの再レンダリングをしないようにできる。<br></br>但し、関数をpropsで渡す場合は再レンダリングされるので、その場合も<Link href={'/06_useCallback'}>useCallback</Link>も併用する。
+          memoを使うと子コンポーネントで参照する値（props）が更新されない限り子コンポーネントの再レンダリングをしないようにできる。<br></br>但し、関数をpropsで渡す場合は再レンダリングされるので、その場合は<Link href={'/06_useCallback'}>useCallback</Link>も併用する。
+        </p>
+      </details>
+      <details className='accordion-003 fs_small '>
+        <summary>memoの注意点</summary>
+        <p className='m-0'>
+          memo化できるのは【<b>コンポーネント</b>】のみ
         </p>
       </details>
       <div className='card'>
